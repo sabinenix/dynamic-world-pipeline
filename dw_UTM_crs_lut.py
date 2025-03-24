35b50c0b5bccc9322fc93c69e8258e0726dcc475
hooge104 <vandenhoogen.johan@gmail.com>
2025-03-24 20:23:38 +0100

    fix oversight in crs fetching

--- dw_UTM_crs_lut.py
+++ dw_UTM_crs_lut.py
@@ -6,7 +6,11 @@ ee.Initialize()
 
 # Get DW image collection for a random year
 DW = (ee.ImageCollection('GOOGLE/DYNAMICWORLD/V1')
-    .filter(ee.Filter.date('2019-01-01', '2019-12-31')))
+    .filter(ee.Filter.date('2019-01-01', '2019-12-31'))
+    .map(lambda image: image.set('crs', image.projection().crs()))
+    )
+
+print(DW.first().get('crs').getInfo())
 
 # Create list of all UTM epsg codes; 
 # UTM codes range from 1 to 60, starting with 326 in the northern hemisphere and 327 in the southern hemisphere
@@ -17,7 +21,11 @@ def get_affine_transform(utm_epsg_code):
     print(f"Getting affine transform for {utm_epsg_code}")
     img = DW.filter(ee.Filter.eq('crs', utm_epsg_code)).first().getInfo()
 
-    transform = str(img['bands'][0].get('crs_transform', None))
+    transform = img['bands'][0].get('crs_transform', None)
+
+    # Manually correct second scale parameter in transform; should be negative [10, 0, 390450, 0, 10, 6090450]
+    transform = transform[:4] + [-abs(transform[4])] + transform[5:]
+
     UTM_zone = img['properties'].get('system:index', None).split('_')[-1]
 
     return {
@@ -35,3 +43,6 @@ lut = pd.DataFrame(affine_transforms).T
 
 # Save dataframe to csv
 lut.to_csv('dw_UTM_crs_lut.csv', index=False)
+
+# test for first row
+utm_epsg_code = utm_epsg_codes[1]
